653e70baf4679162e1443cc685ef55aae88a82e8
Maarten Olijve <maarten@brixion.com>
2022-05-31 11:28:31 +0200

    🎨 Formatting

--- tests/GS1Test.php
+++ tests/GS1Test.php
@@ -10,32 +10,32 @@ use Brixion\Bardecoder\UdiDecoder;
 final class GS1Test extends TestCase
 {
     private array $gs1 = [
-            [
-                "gs1" => "010019506205432717260806108356271",
-                "product_code" => "00195062054327",
-                "expiry_date" => "2026-08-06",
-                "lot" => "8356271"
-            ],
-            [
-                "gs1" => "01007073874690851724072210202407JV",
-                "product_code" => "00707387469085",
-                "expiry_date" => "2024-07-22",
-                "lot" => "202407JV"
-            ],
-            [
-                "gs1" => "01008170510223211121120210A63098",
-                "product_code" => "0081705102232",
-                "expiry_date" => "2023-11-04",
-                "lot" => "A63098"
-            ]
-        ];
+        [
+            "gs1" => "010019506205432717260806108356271",
+            "product_code" => "00195062054327",
+            "expiry_date" => "2026-08-06",
+            "lot" => "8356271"
+        ],
+        [
+            "gs1" => "01007073874690851724072210202407JV",
+            "product_code" => "00707387469085",
+            "expiry_date" => "2024-07-22",
+            "lot" => "202407JV"
+        ],
+        [
+            "gs1" => "01008170510223211121120210A63098",
+            "product_code" => "0081705102232",
+            "expiry_date" => "2023-11-04",
+            "lot" => "A63098"
+        ]
+    ];
 
 
 
     /** @test */
     public function it_can_instantiate_gs1(): void
     {
-        foreach($this->gs1 as $gs1){
+        foreach ($this->gs1 as $gs1) {
             $decoder = new UdiDecoder($gs1["gs1"]);
             $this->assertInstanceOf(UdiDecoder::class, $decoder);
         }
@@ -43,7 +43,7 @@ final class GS1Test extends TestCase
     /** @test */
     public function it_returns_gtin_from_gs1(): void
     {
-        foreach($this->gs1 as $gs1){
+        foreach ($this->gs1 as $gs1) {
             $decoder = new UdiDecoder($gs1["gs1"]);
             $this->assertEquals($gs1["product_code"], $decoder->product_code);
         }
@@ -51,7 +51,7 @@ final class GS1Test extends TestCase
     /** @test */
     public function it_returns_expiry_date_from_gs1(): void
     {
-        foreach($this->gs1 as $gs1){
+        foreach ($this->gs1 as $gs1) {
             $decoder = new UdiDecoder($gs1["gs1"]);
             $this->assertEquals($gs1["expiry_date"], $decoder->expiry_date);
         }
@@ -59,7 +59,7 @@ final class GS1Test extends TestCase
     /** @test */
     public function it_returns_lot_from_gs1(): void
     {
-        foreach($this->gs1 as $gs1){
+        foreach ($this->gs1 as $gs1) {
             $decoder = new UdiDecoder($gs1["gs1"]);
             $this->assertEquals($gs1["lot"], $decoder->lot);
         }
